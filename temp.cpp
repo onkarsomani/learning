abc12b259048cdf76e67b3717fe8fe60791c0d29
onkarsomani <102052639+onkarsomani@users.noreply.github.com>
2025-02-05 12:47:07 +0000

    aadha code

--- temp.cpp
+++ temp.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long int
+#define double long double
+#define ull unsigned long long
 #define MOD 998244353
 #define print(a) for(auto x : a) cerr << x << " "; cerr<<endl;
 #define print1(b) for(auto x : b) cerr << x.first << " " << x.second <<endl;
@@ -15,12 +18,6 @@ void __f(const char * names , Arg1&& arg1, Args&&... args){
     cerr.write (names, comma - names) << " : " << arg1 << " | ";__f(comma +1, args... );
 }
 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds; 
-  
-#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update> 
-#define int long long int
 
 
 int32_t main() {
@@ -30,9 +27,93 @@ int32_t main() {
     int mod = 998244353;
     cin>>ttt;
     for(int tt = 1 ; tt <= ttt ; tt++ ){
+        int n;
+        string s1 , s2;
+        cin  >> s1 >> s2;
+        n = s1.length();
+        vector<int> cnt1 (2, 0) , cnt2 (2,0);
+        int j = 0;
+        int flg = 0;
+        bug(s1, s2);
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(s1[i] == '0')cnt1[0]++;
+            else cnt1[1]++;
+            if(s2[i] == '0')cnt2[0]++;
+            else cnt2[1]++;
+            // print(cnt1);
+            // print(cnt2);
+            if(cnt1[0] == cnt2[0] && cnt1[1] == cnt2[1]){
+                bug(i);
+                if(j == i)j++;
+                else{
+                    if( (j - 1 < 0 || s1[j] != s1[j-1]) &&  (i + 1 >= n || s1[i] != s1[i+1]) ){
+                        queue<int> q [2];
+                        char prev = s2[j];
+                        int cnt = 1;
+                        for(int k = j + 1 ; k <= i ; k++){
+                            if(s2[k] != prev){
+                                q[prev - '0'].push(cnt);
+                                cnt = 1;
+                                prev = s2[k];
+                            }
+                            else cnt++;
+                        }
+                        q[prev - '0'].push(cnt);
+                        int carr[2];
+                        cnt = 1;
+                        carr[0] = 0 , carr[1] = 0;
+                        prev = s1[j];
+                        int tans = 0;
+                        for(int k = j + 1 ; k <= i ; k++){
+                            if(s1[k] != prev){
+                                carr[prev - '0'] += cnt; 
+                                cnt = 1;
+                                if(q[prev - '0'].empty()){
+                                    flg = 1;
+                                    break;
+                                }
+                                else{
+                                    if(carr[prev - '0'] == q[prev - '0'].front())q[prev - '0'].pop();
+                                    else if(carr[prev - '0'] > q[prev - '0'].front()){
+                                        flg = 1;
+                                        break;
+                                    }
+                                }
+                                // redefine prev 
+                                tans ++;
+                                prev = s1[k];
+                            }
+                            else cnt++;
+                        }
+                        tans++;
+                        ans += tans/2;
+                        carr[prev - '0'] += cnt; 
+                        if(q[prev - '0'].empty()){
+                            flg = 1;
+                            break;
+                        }
+                        else{
+                            if(carr[prev - '0'] == q[prev - '0'].front())q[prev - '0'].pop();
+                            else if(carr[prev - '0'] > q[prev - '0'].front()){
+                                flg = 1;
+                                break;
+                            }
+                        }
+                        if(flg)break;
+                        else j = i + 1;
+                    }
+                    else{
+                        flg =1;
+                        break;
+                    }
+                }
+            }
+        }
+        if(j != n)flg = 1;
+        if(flg)cout << -1 << '\n';
+        else cout << ans << '\n';
         
-        
-
     }
 }
 
